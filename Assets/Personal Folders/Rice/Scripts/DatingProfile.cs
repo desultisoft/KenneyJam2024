43f82a0154f39da7c2a5dd4d140917c1ef04907e
RootRice <mod.rice.jagex@gmail.com>
2024-07-20 16:49:34 +0100

    Dating profile infrastructure

--- Assets/Personal Folders/Rice/Scripts/DatingProfile.cs
+++ Assets/Personal Folders/Rice/Scripts/DatingProfile.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class DatingProfile : MonoBehaviour
 {
-    int numRunes;
-    int[] runeTypes;
+    public static DatingProfile datingProfile;
+    public int numRunes;
+    public runes[] runeTypes;
+
+    [SerializeField] Item[] items;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        datingProfile = this;
+    }
+
+    private void Start()
+    {
+        GenerateProfile();
     }
 
     // Update is called once per frame
@@ -21,6 +29,38 @@ public class DatingProfile : MonoBehaviour
 
     void GenerateProfile()
     {
+        numRunes = Random.Range(3, 6);
+        runeTypes = new runes[numRunes];
+        int numItems = items.Length;
+        int randomIndex = Random.Range(0, numItems);
+        switch (numRunes)
+        {
+            case 3:
+                runeTypes[0] = items[randomIndex].runes[0];
+                randomIndex = LoopIndex(randomIndex + Random.Range(0, numItems), numItems);
+                runeTypes[1] = items[randomIndex].runes[0];
+                randomIndex = LoopIndex(randomIndex + Random.Range(0, numItems), numItems);
+                runeTypes[2] = items[randomIndex].runes[0];
+                return;
+            case 4:
+                runeTypes[0] = items[randomIndex].runes[0];
+                runeTypes[1] = items[randomIndex].runes[1];
+                randomIndex = LoopIndex(randomIndex + Random.Range(0, numItems), numItems);
+                runeTypes[2] = items[randomIndex].runes[0];
+                runeTypes[3] = items[randomIndex].runes[1];
+                return;
+            case 5:
+                runeTypes[0] = items[randomIndex].runes[0];
+                runeTypes[1] = items[randomIndex].runes[1];
+                runeTypes[2] = items[randomIndex].runes[2];
+                runeTypes[3] = items[randomIndex].runes[3];
+                runeTypes[4] = items[randomIndex].runes[4];
+                return;
+        }
+    }
 
+    private int LoopIndex(int index, int max)
+    {
+        return(index % max);
     }
 }
